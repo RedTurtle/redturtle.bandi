1f3a34d9ca0d1ad890dc9f44db56ba914956a41b
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-12-30 10:28:52 +0100

    Preparing release 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of redturtle.bandi
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.2.dev0'
+version = '1.0.2'
 
 setup(
     name='redturtle.bandi',
