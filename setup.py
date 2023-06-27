d7e296803a1601eaef0b217476593334f97154f9
Mauro Amico <mauro.amico@gmail.com>
2023-06-27 16:14:08 +0200

    Preparing release 1.4.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of redturtle.bandi
 import os
 from setuptools import setup, find_packages
 
-version = "1.4.3.dev0"
+version = "1.4.3"
 
 setup(
     name="redturtle.bandi",
