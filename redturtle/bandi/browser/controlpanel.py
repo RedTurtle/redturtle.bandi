89f73260e144a4460223217f4424cc564c972cf1
Luca Bellenghi <lucabel@gmail.com>
2024-12-04 08:44:18 +0100

    update it trasnlations

--- redturtle/bandi/browser/controlpanel.py
+++ redturtle/bandi/browser/controlpanel.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from plone.app.registry.browser import controlpanel
 from redturtle.bandi.interfaces.settings import IBandoSettings
+from redturtle.bandi import bandiMessageFactory as _
 
 
 class BandiSettingsForm(controlpanel.RegistryEditForm):
     schema = IBandoSettings
     id = 'BandiSettingsForm'
-    label = u"Impostazioni per i bandi"
+    label = _("bandi_settings_label", default="Impostazioni per i bandi")
 
 
 class BandiControlPanel(controlpanel.ControlPanelFormWrapper):
