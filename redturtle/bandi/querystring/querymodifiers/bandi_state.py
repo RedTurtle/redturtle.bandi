85fe135e938af690c5ca623c21745f45fc3a37a9
Roman Kysil <roman.kysil@redturtle.it>
2025-01-22 14:52:33 +0100

    Squashed commit of the following:
    commit aa2b92d0a176c08476c86bee315bfbae51d436ac
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Wed Jan 22 14:50:55 2025 +0100
    
        Intermediate commit
    
    commit f22630251c3fcc383176666cd3f2c15b06352d81
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Wed Jan 22 11:39:12 2025 +0100
    
        Intermediate commit
    
    commit dd52d3ccc796c3064bdd03a38b6366617bc3ca84
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Wed Jan 22 11:33:32 2025 +0100
    
        Intermediate commit
    
    commit feb34aaaea3f424f5e206d2b71f998532dafedc8
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Wed Jan 22 10:38:11 2025 +0100
    
        Fix voc
    
    commit b5049d6912ccba96f77fbb4b6f55be6c6f7724ab
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Wed Jan 22 09:51:18 2025 +0100
    
        Intermediate commit
    
    commit 97674b11a1d804c1449fea7d1dc3c6536bf8f132
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Tue Jan 21 17:36:04 2025 +0100
    
        more criteria
    
    commit e0817632911fcf67819828d4752afd734956801a
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Tue Jan 21 17:20:21 2025 +0100
    
        Update upgrades.zcml
    
    commit 30eecb41fb5e947e8fad28327057a7b26b214fe3
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Tue Jan 21 17:16:48 2025 +0100
    
        Update metadata.xml
    
    commit f4b60be434c8e0d5e4846dac7f78fd731522e944
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Tue Jan 21 17:02:38 2025 +0100
    
        Update registry.xml
    
    commit 6507a294b50e3f0ee9c733eaa97fd5cce22c4577
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Mon Jan 20 17:48:22 2025 +0100
    
        Intermediate commit
    
    commit 7eeea29b03faac33467a05eb77b8ab1a3aeb9bfe
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Mon Jan 20 17:32:56 2025 +0100
    
        Initial implementation
    
    commit 203687c34d3bbbb08120fa23e3991090fb9ad848
    Author: Roman Kysil <roman.kysil@redturtle.it>
    Date:   Mon Jan 20 15:18:25 2025 +0100
    
        Initial implementation

---
+++ redturtle/bandi/querystring/querymodifiers/bandi_state.py
@@ -0,0 +1,68 @@
+from plone.app.querystring.interfaces import IQueryModifier
+from zope.interface import provider
+from plone.restapi.serializer.converters import json_compatible
+from DateTime import DateTime
+
+
+@provider(IQueryModifier)
+def modify_bandi_state_query(query):
+    now = json_compatible(DateTime())
+    item = None
+    query_extender = []
+
+    state_operators = {
+        "open": (
+            {
+                "o": "plone.app.querystring.operation.date.afterDateTime",
+                "v": now,
+                "i": "scadenza_bando",
+            },
+            {
+                "o": "plone.app.querystring.operation.date.afterDateTime",
+                "v": now,
+                "i": "chiusura_procedimento_bando",
+            },
+        ),
+        "in-progress": (
+            {
+                "o": "plone.app.querystring.operation.date.beforeDateTime",
+                "v": now,
+                "i": "scadenza_bando",
+            },
+            {
+                "o": "plone.app.querystring.operation.date.afterDateTime",
+                "v": now,
+                "i": "chiusura_procedimento_bando",
+            },
+        ),
+        "closed": (
+            {
+                "o": "plone.app.querystring.operation.date.beforeDateTime",
+                "v": now,
+                "i": "chiusura_procedimento_bando",
+            },
+        ),
+        "scheduled": (
+            {
+                "o": "plone.app.querystring.operation.date.afterDateTime",
+                "v": now,
+                "i": "apertura_bando",
+            },
+        ),
+    }
+
+    for i in query:
+        if i.get("i", "") == "bando_state":
+            item = i
+
+            for value in i.get("v", []):
+                operator = state_operators.get(value, None)
+
+                if operator:
+                    query_extender.extend(operator)
+
+    if item:
+        query.remove(item)
+        query.extend(query_extender)
+
+    return query
