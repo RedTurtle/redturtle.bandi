ce883c2b42201273b6ba9d16484db2e7bf5927d7
Andrea Cecchi <andrea.cecchi85@gmail.com>
2025-03-12 10:35:00 +0100

    Back to development: 1.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of redturtle.bandi
 import os
 from setuptools import setup, find_packages
 
-version = "1.5.1"
+version = "1.5.2.dev0"
 
 setup(
     name="redturtle.bandi",
